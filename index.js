b54a5f3f1230fd69b581f36f02c92f826597ab40
Qurram Zaheer <qurramzaheer@pop-os.localdomain>
2020-06-26 04:04:52 +0530

    refactoring for efficiency

--- index.js
+++ index.js
@@ -7,28 +7,42 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+/* --------- Helper function for parsing and merging both data files -------- */
+/* ---- Save computation on frontend by seperating chart and product data --- */
+const mergeProdAndInvData = (productData, invData) => {
+  productData = JSON.parse(productData);
+  invData = JSON.parse(invData);
+  productData.forEach(
+    (product) => (product["inv_data"] = invData[product.product_id])
+  );
+  return productData;
+};
+
+/* ------- This endpoint provides combined data of products and charts ------ */
+
 app.get("/product-list", (req, res) => {
-  const fileContents = fs.readFileSync("data.txt", "utf-8");
-  res.json(JSON.parse(fileContents));
+  let productData = fs.readFileSync("product_data.txt", "utf-8");
+
+  let invData = fs.readFileSync("inv_data.txt", "utf-8");
+
+  res.json(mergeProdAndInvData(productData, invData));
 });
 
+/* ------------------ Endpoint for editing inventory value ------------------ */
+
 app.post("/edit-product/:id", (req, res) => {
-  let fileContents = fs.readFileSync("data.txt", "utf-8");
-  fileContents = JSON.parse(fileContents);
+  let invData = fs.readFileSync("inv_data.txt", "utf-8");
+  invData = JSON.parse(invData);
+  let productData = fs.readFileSync("product_data.txt", "utf-8");
 
-  console.log(req.body, req.params.id);
-  console.log(fileContents);
-  const foundItem = fileContents.find(
-    (item) => item.product_id === parseInt(req.params.id)
-  );
-
-  foundItem["inv_data"][
-    foundItem["inv_data"].length - 1
+  // Set last element of invData array(which is the inventory value at the most recent date)
+  //to the updated value provided by client.
+  invData[req.params.id][
+    invData[req.params.id].length - 1
   ].inventoryVal = parseInt(req.body.newVal);
-  console.log(foundItem["inv_data"]);
-  // fileContents[foundIndex] = req.body;
-  fs.writeFileSync("data.txt", JSON.stringify(fileContents));
-  return res.json(fileContents);
+
+  fs.writeFileSync("inv_data.txt", JSON.stringify(invData));
+  return res.json(mergeProdAndInvData(productData, JSON.stringify(invData)));
 });
 
 app.get("/", (req, res) => {
--- temp.js
+++ temp.js
@@ -1,57 +1,57 @@
+/* -------------------------- DO NOT RUN THIS FILE, ------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*           IT WAS ONLY USED FOR INITIALIZING THE APPLICATION  DATA          */
+/* -------------------------------------------------------------------------- */
+
 const fs = require("fs");
 
-// let data = [
-//   {
-//     product_id: 1,
-//     product_name: "iPhone 11",
-//   },
-//   {
-//     product_id: 2,
-//     product_name: "Samsung S20",
-//   },
-//   {
-//     product_id: 3,
-//     product_name: "Huawei P20",
-//   },
-//   {
-//     product_id: 4,
-//     product_name: "OnePlus 8",
-//   },
-//   {
-//     product_id: 5,
-//     product_name: "HTC One",
-//   },
-//   {
-//     product_id: 6,
-//     product_name: "Samsung Note",
-//   },
-//   {
-//     product_id: 7,
-//     product_name: "iPad Pro",
-//   },
-// ];
+let data = [
+  {
+    product_id: 1,
+    product_name: "iPhone 11",
+  },
+  {
+    product_id: 2,
+    product_name: "Samsung S20",
+  },
+  {
+    product_id: 3,
+    product_name: "Huawei P20",
+  },
+  {
+    product_id: 4,
+    product_name: "OnePlus 8",
+  },
+  {
+    product_id: 5,
+    product_name: "HTC One",
+  },
+  {
+    product_id: 6,
+    product_name: "Samsung Note",
+  },
+  {
+    product_id: 7,
+    product_name: "iPad Pro",
+  },
+];
 
-// fs.writeFile("data.txt", JSON.stringify(data), (err) =>
-//   err ? console.log(err) : console.log("Working")
-// );
+fs.writeFile("data.txt", JSON.stringify(data), (err) =>
+  err ? console.log(err) : console.log("Working")
+);
 
 let data = fs.readFileSync("data.txt", "utf-8");
 data = JSON.parse(data);
 console.log(data);
 
+const data2 = {};
+
 data.forEach((item) => {
-  let arr = new Array(20);
-  let initDate = "March 4, 2020";
-  for (let i = 0; i < arr.length; i++) {
-    let tempDate = Date.parse(initDate) + i * 24 * 60 * 60 * 1000;
-    arr[i] = {};
-    arr[i]["date"] = tempDate;
-    arr[i]["inventoryVal"] = Math.floor(Math.random() * 1000) + 1;
-  }
-  item["inv_data"] = arr;
-  console.log(arr);
+  data2[item.product_id] = item["inv_data"];
+  delete item["inv_data"];
+  console.log(item);
 });
 
-fs.writeFile("data.txt", JSON.stringify(data), (err) =>
-  err ? console.log(err) : console.log
-);
+fs.writeFileSync("product_data.txt", JSON.stringify(data));
+fs.writeFileSync("inv_data.txt", JSON.stringify(data2));
